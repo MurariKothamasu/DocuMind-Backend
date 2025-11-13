eb18bc367f063e5f1d01ac93d5e2848f7c0092b9
Murari <kothamasuchandrasai56@gmail.com>
2025-11-13 18:12:47 +0530

    feat: Removed MongoDb Url From Git

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-// mongoose.connect(
-//   "mongodb+srv://KothamasuChandrasai:Chandrasai%40192003@practicecluster.izsipds.mongodb.net/"
-// ).then(console.log("Connection successfull"))
+
 
 async function connectDB() {
   await mongoose.connect(process.env.DB_CONNECTION_SECRET);
